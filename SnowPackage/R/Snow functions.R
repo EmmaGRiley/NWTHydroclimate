d0264f97a5f568797db70beab45a50a3d1d77b8c
Emma R <132517733+EmmaGRiley@users.noreply.github.com>
2023-05-05 12:42:14 -0600

    Update Snow functions.R

--- SnowPackage/R/Snow functions.R
+++ SnowPackage/R/Snow functions.R
@@ -14,7 +14,8 @@
 #'Takes snow survey values from the SnowDB database and generates a summary table for the current year.
 #'Output is an excel file written as "filepath" argument.
 #'swe, depth and site variables must be named "SWE", "depth" and "site" in the input dataframe. 
-#'@param x Snow survey dataframe
+#'@param x Snow survey dataframe - measurement sheet
+#'@param y Snow survey dataframe - metadata sheet
 #'@param surface Filters for surface type. Specify "upland", "lake" or NULL (all surface types). 
 #'@param minyear Minimum calculation year (minus 1) for the normal period. For example, for a 2001-2020 normal period minyear is set to 2000.
 #'@param maxyear Maximum calculation year (plus 1) for the normal period. For example, for a 2001-2020 normal period maxyear is set to 2021.
@@ -23,12 +24,26 @@
 #'@param filepath Specify filepath for output excel file including desired file name. For example "~/SummaryTableSO.xlsx"
 #'@export
 #'
-SummaryTableSO <- function(x, surface, minyear, maxyear, curmaxyear, act, filepath)
+SummaryTableSO <- function(x, y, surface, minyear, maxyear, curmaxyear, act, filepath)
   
-{ST_means = x %>%
-  dplyr::mutate(isY = dplyr::case_when(dataflags=="Y" ~ 'Y')) %>%
+{appendvar = function (var1, var2, var3){
+  mtch = match(var1, var2)
+  var4<-var3[match(var1, var2)]
+  return(var4)}
+ 
+  region = appendvar(var1=x$site, var2 = y$Site_name, var3 = y$region)
+  activity = appendvar(var1=x$site, var2 = y$Site_name, 
+                     var3 = y$`Active/Inactive`)
+  cf = appendvar(var1=x$site, var2 = y$Site_name, 
+               var3 = y$`catchment reference`)
+  lng<-appendvar(x$site, y$Site_name, y$Long)
+  lt<-appendvar(x$site, y$Site_name, y$Lat)
+  x_2<-cbind(x, region, activity, cf, lng, lt) 
+ 
+  ST_means = x_2 %>%
+  dplyr::mutate(x_2, isY = dplyr::case_when(dataflags=="Y" ~ 'Y')) %>%
   dplyr::filter(surf==surface, 
-                is.na(x$isY),
+                is.na(isY),
                 activity==act) %>%
   dplyr::group_by(site, year) %>%
   dplyr::summarize(yearlySWE = round(mean(SWE, na.rm=TRUE), 4),
