3aeacf30abb84090adfe02359b0fcbaf1c50fa39
Emma G <132517733+EmmaGRiley@users.noreply.github.com>
2025-10-08 14:03:28 -0600

    Update tb_source_functions.R

--- Transboundary/tb_source_functions.R
+++ Transboundary/tb_source_functions.R
@@ -7,11 +7,11 @@
  user <- paste0("C:/Users/", tolower(Sys.getenv("USERNAME")))
  wf_path <- paste0(user, "R_Scripts/Functions/Workflows/")
 
-#Path to additional R scripts 
+#Path to additional R scripts (optional)
  add_R_path <- paste0(user, "/Documents/NT_Hydrology/R/")
 
 # Define file paths
-#wf_path <- paste0(user, "/Documents/R_Scripts/Functions/Workflows/")
+wf_path <- paste0(user, "/Documents/R_Scripts/Functions/Workflows/")
 package_path <- paste0(user, "/Documents/R_Scripts/Packages/")
 nwtclimate_path <- paste0(package_path, "nwtclimate/")
 hydroclim_path <- paste0(package_path, "hydroclim/")
@@ -31,12 +31,12 @@ merged_data = paste0(original_data, "_Merged")
 #extension <- ".rds"
 
 # Call functions from other worksheets into active environment
-#source(paste0(nwtclimate_path, R_path, "dependencies_data.R"))
+source(paste0(nwtclimate_path, R_path, "dependencies_data.R"))
 source(paste0(nwtclimate_path, R_path, "dependencies_functions.R"))
-#source(paste0(nwtclimate_path, R_path, "update.R"))
-#source(paste0(nwtclimate_path, R_path, "filter.R"))
-#source(paste0(nwtclimate_path, R_path, "stitch.R"))
-#source(paste0(nwtclimate_path, R_path, "nt_merge.R"))
+source(paste0(nwtclimate_path, R_path, "update.R"))
+source(paste0(nwtclimate_path, R_path, "filter.R"))
+source(paste0(nwtclimate_path, R_path, "stitch.R"))
+source(paste0(nwtclimate_path, R_path, "nt_merge.R"))
 source(paste0(hydroclim_path, R_path, "hydroclim_load.R"))
 source(paste0(hydroclim_path, R_path, "hydro_filter.R"))
 source(paste0(hydroclim_path, R_path, "hydro_compile_daily.R"))
@@ -61,9 +61,10 @@ source(paste0(hydroclim_path, R_path, "clim_plot_dayofyear.R"))
 #source(paste0(wf_path, "INF_plots.R"))
 source(paste0(tb_path, "tb_calc_triggers.R"))
 source(paste0(tb_path, "tb_plot_triggers.R"))
-source(paste0(add_R_path, "SOEfigs_functions.R"))
+source(paste0(wf_path, "SOEfigs_functions.R"))
 
 ###########################################################################################################
+
 
 
 
